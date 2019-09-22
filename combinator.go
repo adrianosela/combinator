758eb63fed2ae456bab2f46b50875236fb932009
Adriano <adriano.selaviles@gmail.com>
2019-09-21 19:27:34 -0700

    add alphanumeric charset

--- combinator.go
+++ combinator.go
@@ -12,6 +12,17 @@ type Combinator struct {
 	currentKey []byte
 }
 
+// AlphanumericCharset represents a full alphanumeric charset
+var AlphanumericCharset = []byte{
+	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+	'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+	'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
+	'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+	'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+	'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7',
+	'8', '9',
+}
+
 // NewCombinator is the constructor for the combinator object
 func NewCombinator(charset []byte) (*Combinator, error) {
 	if len(charset) < 1 {
